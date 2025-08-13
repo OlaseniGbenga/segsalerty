1fbb8f244c609c76fbe0af39ae0acb4b482e1d5b
Gbenga Olaseni <gj.olaseni@gmail.com>
2025-08-13 11:32:44 +0100

    completed section2, started section3

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from "@/component/sections/hero";
+import Section2 from "@/component/sections/section2";
 import { Badge } from "@/component/ui/badge";
 import Image from "next/image";
 
@@ -6,51 +7,78 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <div className="px-[20px] md:px-[60px] py-[80px] flex flex-col items-center">
-        <p className="text-[32px] font-normal text-center">
-          We take pride in our distinctive approach: rather than directly
-          teaching <br /> technical skills,{" "}
-          <span className="font-bold">
-            we focus on unleashing the inherent potential within
-            <br /> talented tech enthusiasts.
-          </span>
-        </p>
-        <Badge>OUR UNIQUE IMPACT</Badge>
-        <div className="bg-[#E6F2EF] flex gap-[40px] px-[40px] rounded-[10px] flex-col md:flex-row ">
-          <div className="py-[30px] px-[16px] flex flex-col items-center">
-            <Image
-              src="/section2/UsersFour.svg"
-              width={48}
-              height={48}
-              alt="Team work"
-            />
-            <p className="font-bold text-[48px]">1000+</p>
-            <p className="font-normal text-[18px]"> Tech Talents Transformed</p>
+      <Section2 />
+      <section className="flex  flex-col">
+        <Badge className="self-center">MEET OUR FOUNDER</Badge>
+        <div className="px-[20px] md:px-[60px] flex flex-col lg:flex-row gap-[24] lg:gap-0 justify-between">
+          <div className=" flex flex-col gap-[24]">
+            <div className="flex flex-col gap-[8px]">
+              <p className="text-deepteal font-bold text-[18px]">
+                VISIONARY LEADERSHIP
+              </p>
+              <p className="text-[18px] font-normal text-black">
+                <span className="font-bold">
+                  Segun Mustafa is a seasoned CTO and Software Engineering
+                  leader
+                </span>
+                with <br /> over 15 years of experience building and scaling
+                enterprise systems — <br />
+                particularly in business automation and fintech.
+              </p>
+            </div>
+            <div className="flex flex-col gap-[8px]">
+              <p className="text-deepteal font-bold text-[18px]">
+                MASTER OF HIS CRAFT
+              </p>
+              <p className="text-[18px] font-normal text-black">
+                A certified Oracle Java Master since 2011, Segun also holds a
+                BTech in
+                <br /> Information Technology and multiple technical
+                certifications. His
+                <br /> expertise spans high-performance systems, cloud-native
+                architecture,
+                <br /> and scalable platforms used across continents.{" "}
+              </p>
+            </div>
+            <div className="flex flex-col gap-[8px]">
+              <p className="text-deepteal font-bold text-[18px]">
+                BEYOND TECHNICAL EXCELLENCE
+              </p>
+              <p className="text-[18px] font-normal text-black">
+                What sets Segun apart is his unwavering commitment to giving
+                back.
+                <br /> Rather than simply leading from the top, he’s built an
+                ecosystem that <br /> empowers aspiring African tech talents
+                through mentorship, real-world
+                <br /> projects, and access to global tech leaders.{" "}
+              </p>
+            </div>
+            <div className="flex flex-col gap-[8px]">
+              <p className="text-deepteal font-bold text-[18px]">
+                BUILDING A MOVEMENT
+              </p>
+              <p className="text-[18px] font-normal text-black">
+                He’s not just developing engineers — he’s fostering a culture of
+                <br />
+                excellence, inclusion, and impact. Through his leadership, a new
+                <br />
+                generation of African tech professionals is rising — bold,
+                equipped, and
+                <br /> ready to lead.
+              </p>
+            </div>
           </div>
 
-          <div className="py-[30px] px-[16px] flex flex-col items-center">
-           <Image
-              src="/section2/Target.svg"
-              width={48}
-              height={48}
-              alt="Team work"
-            />
-            <p className="font-bold text-[48px]">10+</p>
-            <p className="font-normal text-[18px]"> Years of impact</p>
-          </div>
-
-          <div className="py-[30px] px-[16px] flex flex-col items-center">
-            <Image
-              src="/section2/Handshake.svg"
-              width={48}
-              height={48}
-              alt="Team work"
-            />
-            <p className="font-bold text-[48px]">50+</p>
-            <p className="font-normal text-[18px]"> Collaborative Projects</p>
-          </div>
+          <Image
+            src="/segun.png"
+            width={628}
+            height={490}
+            alt="Team wok"
+            className="w-full md:w-[628px] h-auto"
+          />
         </div>
-      </div>
+      </section>
     </>
   );
 }
+//https://www.figma.com/design/jYLhtMYidPyTMQFPhuNep6/SEGSALERTY?node-id=183-818&t=uSN6N0F18SRIBmk9-0
---
+++ component/sections/section2.tsx
@@ -0,0 +1,122 @@
+import { Badge } from "@/component/ui/badge";
+import Image from "next/image";
+
+const Section2 = () => {
+  return (
+    <section className="py-[80px] flex flex-col gap-[80px]">
+      <p className="text-[32px] font-normal text-center px-[20px] md:px-[60px]">
+        We take pride in our distinctive approach: rather than directly teaching{" "}
+        <br /> technical skills,{" "}
+        <span className="font-bold">
+          we focus on unleashing the inherent potential within
+          <br /> talented tech enthusiasts.
+        </span>
+      </p>
+      <div className="  flex flex-col  gap-[80px]">
+        <Badge className="self-center">OUR UNIQUE IMPACT</Badge>
+        <div className="self-center bg-[#E6F2EF] flex gap-[40px] px-[40px] rounded-[10px] flex-col md:flex-row ">
+          <div className="py-[30px] px-[16px] flex flex-col items-center">
+            <Image
+              src="/section2/UsersFour.svg"
+              width={48}
+              height={48}
+              alt="Team work"
+            />
+            <p className="font-bold text-[48px]">1000+</p>
+            <p className="font-normal text-[18px]"> Tech Talents Transformed</p>
+          </div>
+
+          <div className="py-[30px] px-[16px] flex flex-col items-center">
+            <Image
+              src="/section2/Target.svg"
+              width={48}
+              height={48}
+              alt="Team work"
+            />
+            <p className="font-bold text-[48px]">10+</p>
+            <p className="font-normal text-[18px]"> Years of impact</p>
+          </div>
+
+          <div className="py-[30px] px-[16px] flex flex-col items-center">
+            <Image
+              src="/section2/Handshake.svg"
+              width={48}
+              height={48}
+              alt="Team work"
+            />
+            <p className="font-bold text-[48px]">50+</p>
+            <p className="font-normal text-[18px]"> Collaborative Projects</p>
+          </div>
+        </div>
+        <div className="px-[20px] md:px-[60px] flex flex-col lg:flex-row gap-[24] lg:gap-0 justify-between">
+          <div className=" flex flex-col gap-[24]">
+            <div className="flex flex-col gap-[8px]">
+              <p className="text-deepteal font-bold text-[18px]">
+                OUR APPROACH
+              </p>
+              <p className="text-[18px] font-normal text-black">
+                Through mentorship, motivation, and strategic guidance, we help{" "}
+                <br />
+                talented individuals transform into outstanding tech
+                professionals.
+                <br />
+                Our strength lies in our community-first approach, where real
+                growth
+                <br /> happens through shared learning and accountability.
+              </p>
+            </div>
+            <div className="flex flex-col gap-[8px]">
+              <p className="text-deepteal font-bold text-[18px]">
+                &quot;THE MACHINE SPIRIT&quot;
+              </p>
+              <p className="text-[18px] font-normal text-black">
+                We’ve built what our members call the &quot;Machine Spirit&quot;
+                a culture of
+                <br /> relentless excellence, problem-solving, and world-class
+                work ethic. It’s
+                <br /> not just about skills. It’s about becoming the kind of
+                person who stands
+                <br /> out anywhere.
+              </p>
+            </div>
+            <div className="flex flex-col gap-[8px]">
+              <p className="text-deepteal font-bold text-[18px]">
+                FROM POTENTIAL TO POWER
+              </p>
+              <p className="text-[18px] font-normal text-black">
+                Our members don’t just level up in tech.
+                <br />
+                They lead with clarity, solve with confidence, and thrive in
+                every
+                <br />
+                challenge professionally and personally.{" "}
+              </p>
+            </div>
+          </div>
+
+          <Image
+            src="/unique-impact.png"
+            width={628}
+            height={490}
+            alt="Team wok"
+            className="w-full md:w-[628px] h-auto"
+          />
+        </div>
+      </div>
+
+      <p className="px-[20px] md:px-[60px] text-[32px] font-normal text-center">
+        &quot;Our community doesn&apos;t just create developers or designers –{" "}
+        <span className="font-bold">
+          we nurture <br />
+          individuals who approach every challenge with the &apos;Machine
+          Spirit&apos;,
+        </span>{" "}
+        turning
+        <br /> them into exceptional problem solvers and leaders in their
+        fields.&quot;
+      </p>
+    </section>
+  );
+};
+
+export default Section2;
